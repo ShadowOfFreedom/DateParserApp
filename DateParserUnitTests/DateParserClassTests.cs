2330f193d125da45330fe846ca0c230fa8e00d33
Wojciech Wojtkielewicz <wojtkielewicz.wojciech@gmail.com>
2019-06-09 21:06:36 +0200

    Adding unit test and reconfiguration of NUnit

--- DateParserUnitTests/DateParserClassTests.cs
+++ DateParserUnitTests/DateParserClassTests.cs
@@ -1,18 +1,78 @@
+using System;
+using DateParserLib;
 using NUnit.Framework;
 
-namespace Tests
+namespace DateParserUnitTests
 {
-	public class Tests
+	public class TeDateParserClassTestssts
 	{
-		[SetUp]
-		public void Setup()
+
+		DateTime testDate1 = new DateTime(2017, 1, 1);
+		DateTime testDate2 = new DateTime(2017, 1, 5);
+
+		private const string expectedDateString1 = "01 - 05.01.2017";
+		private const string expectedDateString2 = "01.01 - 05.02.2017";
+		private const string expectedDateString3 = "01.01.2016 - 05.01.2017";
+		private const string expectedDateString4 = "05.01.2017";
+		
+		[Test]
+		public void DateParserConstructorFirstArgumentWithValidDataTest()
 		{
+			var dateParser = new DateParser("01.01.2017", "05.01.2017");
+
+			Assert.AreEqual(testDate1,dateParser.FirstDate);
 		}
 
 		[Test]
-		public void Test1()
+		public void DateParserConstructorSecondArgumentWithValidDataTest()
 		{
-			Assert.Pass();
+			var dateParser = new DateParser("01.01.2017", "05.01.2017");
+
+			Assert.AreEqual(testDate2, dateParser.SecondDate);
+		}
+
+		[Test]
+		public void DateParserConstructorWithInvalidDataTest()
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => new DateParser("dd.mm.yyyy", "dd.mm.yyyy"));
+		}
+
+		[Test]
+		public void DateParserConstructorWithNullDataTest()
+		{
+			Assert.Throws<ArgumentNullException>(() => new DateParser(null, ""));
+		}
+
+		[Test]
+		public void DateParseCompareMethodCorrectlyCompareYearAndMonthTest()
+		{
+			var dateParser = new DateParser("01.01.2017", "05.01.2017");
+
+			Assert.AreEqual(expectedDateString1, dateParser.Compare());
+		}
+
+		[Test]
+		public void DateParseCompareMethodCorrectlyCompareMonthTest()
+		{
+			var dateParser = new DateParser("01.01.2017", "05.02.2017");
+
+			Assert.AreEqual(expectedDateString2, dateParser.Compare());
+		}
+
+		[Test]
+		public void DateParseCompareMethodCorrectlyReturnsFullStringTest()
+		{
+			var dateParser = new DateParser("01.01.2016", "05.01.2017");
+
+			Assert.AreEqual(expectedDateString3, dateParser.Compare());
+		}
+
+		[Test]
+		public void DateParseCompareMethodCorrectlyCompareYearAndMonthAndDayTest()
+		{
+			var dateParser = new DateParser("05.01.2017", "05.01.2017");
+
+			Assert.AreEqual(expectedDateString4, dateParser.Compare());
 		}
 	}
 }
