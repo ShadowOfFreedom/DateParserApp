acaf13a1b14a30f501bc5ff24d9e9f954b3ad238
Wojciech Wojtkielewicz <wojtkielewicz.wojciech@gmail.com>
2019-06-09 20:23:10 +0200

    Creating and implementing class DateParser

--- DateParser/DateParser.cs
+++ DateParser/DateParser.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 
-namespace DateParser
+namespace DateParserLib
 {
-	public class Class1
+	public class DateParser
 	{
+		public DateTime FirstDate{ get; private set; }
+		public DateTime SecondDate{ get; private set; }
+
+		public bool DaysAreEqual => FirstDate.Day == SecondDate.Day;
+		public bool MonthsAreEqual => FirstDate.Month == SecondDate.Month;
+		public bool YearsAreEqual => FirstDate.Year == SecondDate.Year;
+
+		public DateParser(string firstDate, string secondDate)
+		{
+			if (string.IsNullOrWhiteSpace(firstDate) || string.IsNullOrWhiteSpace(secondDate))
+				throw new ArgumentNullException("Arguments needs to be date in format dd.mm.yyyy");
+
+			var firstDateArray = firstDate.Split('.', 3);
+			var secondDateArray = secondDate.Split('.', 3);
+
+			try
+			{
+				FirstDate = new DateTime(Convert.ToInt32(firstDateArray[2]), Convert.ToInt32(firstDateArray[1]), Convert.ToInt32(firstDateArray[0]));
+				SecondDate = new DateTime(Convert.ToInt32(secondDateArray[2]), Convert.ToInt32(secondDateArray[1]), Convert.ToInt32(secondDateArray[0]));
+
+			}
+			catch (Exception e)
+			{
+				throw new ArgumentOutOfRangeException("Arguments needs to be date in format dd.mm.yyyy");
+			}
+		}
+
+		public string Compare()
+		{
+			var result = new StringBuilder();
+
+			if(!DaysAreEqual)
+				result.Append(FirstDate.Day.ToString("D2"));
+
+			if (!MonthsAreEqual)
+				result.Append(".").Append(FirstDate.Month.ToString("D2"));
+
+			if (!YearsAreEqual)
+				result.Append(".").Append(FirstDate.Year.ToString("D4"));
+
+			result.Append($" - {SecondDate.ToShortDateString()}");
+
+			return result.ToString();
+		}
 	}
 }
